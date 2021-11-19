ba6e3c52b1784a51d8ada1f2d6c2638b5b74026f
Daniel <ejedenials@gmail.com>
2021-11-19 15:20:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
+from linecache import cache
 import sys
+from types import MethodType
 from psutil import Process,NoSuchProcess
-import argparse
+#import argparse
+import asyncio
+import pickle
+import signal
 
-parser = argparse.ArgumentParser (prog='deBuggy',description='Used For Error Parsing')
-
-parser.add_argument()
-parser.add_argument('--pid', help='Process id of Current Running Python Process')
-parser.add_argument('--foo', help='foo of the %(prog)s program')
-class Debuggy():
+class Buggy():
   def __init__(self):
     self.ProccessId = int(sys.argv[1])
     self.ProcessState = None
     self.ErrorMessage = None
 
+    
 
   def MonitorProcess(self):
     try:
       self.ProcessState = 'alive'
+      
       while self.ProcessState is 'alive':
           TrackProcess = True if Process(self.ProccessId) else False
-    
+
     except NoSuchProcess as e:
           self.ProcessState = 'dead'
+
+    finally:      
           with open('log','r') as log:
-              self.ErrorMessage= log.read()
-              print(self.ErrorMessage) 
+              self.ErrorMessage=log.readlines()
+              print(self.ErrorMessage)
+          pickle_off=  open('cache','rb')    
+          emp=pickle.load(pickle_off)
+          print(emp)    
+  #async def Parser(self):
+   #   res = await self.MonitorProcess() 
+    #stackoverflow
+    #stackexchange
+    #gitcommunity
+
+    #check if error message has been geerated
+    
+ErrorParser = Buggy()
+
+bugZy = ErrorParser.MonitorProcess()
+
+
+
+       
+
+
+#parser = argparse.ArgumentParser (prog='deBuggy',description='Used For Error Parsing')
+#parser.add_argument()
+# parser.add_argument("pid", type=str, help='Process Id of Current Running Python Process')
+# parser.add_argument('-m','--mtr', help='monitor current process',action='store_true')
+# args = parser.parse_args()
+# bugZy = Debuggy(int(args.pid))
+# if args.mtr:
+#   print('monitor: %s' % args.pid)
+#   bugZy.MonitorProcess()
+
+
 
 # syntax_error= """\nForgetting to put a : at the end of an if, elif, else, for, while, class, or def statement. (Causes “SyntaxError: invalid syntax”)
 # \nThis error happens with code like this:
