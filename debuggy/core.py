76345f9699bfdcc99128708ae614e62da302bd2a
Daniel <ejedenials@gmail.com>
2021-12-11 00:42:49 +0100

    Update core.py

--- debuggy/core.py
+++ debuggy/core.py
@@ -21,23 +21,34 @@ def _get_caller_stack(active=False):
 def _get_caller_path():
      # Get the module object of the caller 
     calling_script = inspect.getmodule(_get_caller_stack()[0])
-    #module name from this path
-    caller_path = os.path.dirname(calling_script.__file__)
-    return(caller_path)                
+
+    if calling_script==None:
+       calling_script = inspect.getmodule(_get_caller_stack(active=True)[0])
+    try:
+       #module name from this path
+       caller_path = os.path.dirname(calling_script.__file__)
+    except:
+        print("Debuggy doesn't support the use of interative Shells like idle,ipython,jupyternotebook etc.")
+        sys.exit(1)
+    return (caller_path)                
 
 #print(os.getenv('SHELL'))
 __module_path__ = os.path.dirname(__file__)
 #__python_path__ = sys.executable
 _caller_path = _get_caller_path()    
     #Open Logger
-__logger = open( os.path.join(_caller_path,'log.err'),'w')
-#Get process id of running script
-process_id = os.getpid()
-#Monitor Terminal Output and Capture Standard Error to Logger
-sys.stderr = __logger
-#Run main.py From Open Terminal(path to modules log_file)
-os.system('start cmd /c debuggy call -e %s -id %s'%(os.path.join(_caller_path,'log.err'),process_id))
-#__main = Popen(["python","main.py",str(process_id)],shell=True,stdin=sys.stdin,stdout=sys.stdout,start_new_session=True)#,executable=USERS_DEFAULT_SHELL)    
+try:
+    __logger = open( os.path.join(_caller_path,'log.err'),'w')
+except PermissionError :
+    print('PermissionError, Run script in an administrative terminal')
+else:
+    #Get process id of running script
+    process_id = os.getpid()
+    #Monitor Terminal Output and Capture Standard Error to Logger
+    sys.stderr = __logger
+    #Run main.py From Open Terminal(path to modules log_file)
+    os.system('start cmd /c debuggy call -e %s -id %s'%(os.path.join(_caller_path,'log.err'),process_id))
+    #__main = Popen(["python","main.py",str(process_id)],shell=True,stdin=sys.stdin,stdout=sys.stdout,start_new_session=True)#,executable=USERS_DEFAULT_SHELL)    
     
     
 
