ec050e96ddd0f717e0a6588659993c035c6aa8c8
Daniel <42381829+derhnyel@users.noreply.github.com>
2022-02-24 04:26:41 +0100

    Update __init__.py

--- stalkoverflow/__init__.py
+++
@@ -1,6 +0,0 @@
-import pyfiglet
-import sys
-from stalkoverflow.color import bcolors
-
-DebuggyAnimation = pyfiglet.figlet_format("DeBuggy",font="letters")
-print(bcolors.cyan+DebuggyAnimation+bcolors.end,file=sys.stdout)
